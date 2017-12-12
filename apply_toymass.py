96988364e9f996dab415eedf512e771667fad60b
rfriese <raphael.friese@cern.ch>
2017-12-12 18:10:03 +0100

    update

--- apply_toymass.py
+++ apply_toymass.py
@@ -10,27 +10,34 @@ genmasses = [125, 91]
 for process, genmass in zip(processes, genmasses):
     #process = "DY"
     filename = "data/" + process+".csv"
-    genmass = 125
     #genmass = 91
 
-    dim = 15
+    dim = 12
     n_events = sum(1 for line in open(filename))
     X = np.zeros([n_events, dim])
     svfit = np.zeros([n_events, 4])
+    L = np.zeros([n_events, 4])
+    M = np.zeros([n_events, 4])
     line = 0
     with open(filename, 'rb') as csvfile:
         reader = csv.reader(csvfile, delimiter=';', quotechar='|')
         for row in reader:
-            tmp = [float(i) for i in row]
-            x = tmp[0:11] + [tmp[0]**2, tmp[4]**2, tmp[8]**2, tmp[9]**2]
-            x = np.array(tmp)
+            a = [float(i) for i in row]
+            lepton_1 = FourMomentum(a[4], a[5], a[6], a[7], cartesian=False)
+            lepton_2 = FourMomentum(a[8], a[9], a[10], a[11], cartesian=False)
+            met = FourMomentum(0, a[12], 0, a[13], False)
+            x = np.array([lepton_1.pt+lepton_2.pt, lepton_1.e+lepton_2.e, lepton_1.e/(lepton_1.e+lepton_2.e), lepton_1.pt/(lepton_1.pt+lepton_2.pt), lepton_1.phi, lepton_1.eta, lepton_2.e/(lepton_1.e+lepton_2.e), lepton_2.pt/(lepton_1.pt+lepton_2.pt), lepton_2.phi, lepton_2.eta, met.pt, met.phi])
             X[line,:] = x
-            s = np.array(tmp[11:])
-            svfit[line,:] = s
+            s = FourMomentum(a[0], a[1], a[2], a[3], cartesian=False)
+            svfit[line,:] = np.array([s.pt, s.eta, s.phi, s.m()])
+            l = np.array([lepton_1.e+lepton_2.e, lepton_1.px+lepton_2.px, lepton_1.py+lepton_2.py, lepton_1.pz+lepton_2.pz])
+            L[line,:] = l
+            m = np.array([0, met.px, met.py, 0])
+            M[line,:] = m
             line +=1
 
 
-    from sklearn.preprocessing import PolynomialFeatures
+    #from sklearn.preprocessing import PolynomialFeatures
     #poly = PolynomialFeatures(2)
     #X=poly.fit_transform(X)
 
@@ -47,32 +54,36 @@ for process, genmass in zip(processes, genmasses):
     regressed_phys = scalerTarget.inverse_transform(unscaled_pred)
 
 
-    regressed_fourvectors = [ FourMomentum(a[0], a[1], a[2], a[3]) for a in regressed_phys]
-    regressed_physfourvectors = np.array([ [a.pt, a.eta, a.phi, math.sqrt(a.m2()) if a.m2() > 0 else 0] for a in regressed_fourvectors if a.m2() > 0])
-    #regressed_physfourvectors = np.array([ [a.pt, a.eta, a.phi, math.sqrt(a.m2()) if a.m2() > 0 else 0] for a in regressed_fourvectors])
+
+    energy = np.multiply(regressed_phys[:,0], L[:,0])
+    ones = np.ones([regressed_phys.shape[0]])
+    F = np.subtract( ones,  regressed_phys[:,0])
+    energy /= F
+
+    pz = np.sinh(regressed_phys[:,1]) * np.sqrt( np.square(M[:,1]) + np.square(M[:,2]))
+    
+    regressed_fourvectors = [ FourMomentum(energy[i]+L[i,0], L[i,1]+M[i,1], L[i,2]+M[i,2], L[i,3]+pz[i]) for i in range(regressed_phys.shape[0])]
+    regressed_physfourvectors = np.array([ [a.pt, a.eta, a.phi, math.sqrt(a.m2()) if a.m2() > 0 else 0] for a in regressed_fourvectors])
     regressed_fourvectors = np.array([ [a.e, a.px, a.py, a.pz] for a in regressed_fourvectors])
-    #regressed_fourvectors = np.array([ [a.e, a.px, a.py, a.pz ] for a in regressed_fourvectors])
+    
 
     print process, " toy mean: ", np.mean(regressed_physfourvectors[:,3]), ", toy resolution: ", np.mean(np.abs(regressed_physfourvectors[:,3]-genmass))
     print process, " svfit mean: ", np.mean(svfit[:,3]), ", svfit resolution: ", np.mean(np.abs(svfit[:,3]-genmass))
-    #target_fourvectors = [ FourMomentum(a[0], a[1], a[2], a[3]) for a in target_phys]
-    #target_physfourvectors = np.array([ [a.pt, a.eta, a.phi, math.sqrt(a.m2()) if a.m2() > 0 else 0] for a in target_fourvectors])
-    #target_fourvectors = np.array([ [a.e, a.px, a.py, a.pz ] for a in target_fourvectors])
     for a in range(4):
         pts = plt.figure()
         n, bins, patches = plt.hist(regressed_physfourvectors[:,a], 150, normed=1, facecolor='red', alpha=0.75)
         n, bins, patches = plt.hist(svfit[:,a], 150, normed=1, facecolor='blue', alpha=0.75)
         #n, bins, patches = plt.hist(target_physfourvectors[:,a], 150, normed=1, facecolor='green', alpha=0.75)
-        plt.savefig(process+"-regressed"+str(a)+".pdf")
+        plt.savefig(process+"-regressed"+str(a)+".png")
 
-    for a in range(4):
-        pts = plt.figure()
-        n, bins, patches = plt.hist(unscaled_pred[:,a], 150, normed=1, facecolor='red', alpha=0.75)
+#    for a in range(4):
+#        pts = plt.figure()
+#        n, bins, patches = plt.hist(unscaled_pred[:,a], 150, normed=1, facecolor='red', alpha=0.75)
         #n, bins, patches = plt.hist(target_physfourvectors[:,a], 150, normed=1, facecolor='green', alpha=0.75)
-        plt.savefig(process+"-unscaled"+str(a)+".pdf")
+#        plt.savefig(process+"-unscaled"+str(a)+".pdf")
 
     for a in range(4):
         pts = plt.figure()
         n, bins, patches = plt.hist(regressed_fourvectors[:,a], 150, normed=1, facecolor='red', alpha=0.75)
         #n, bins, patches = plt.hist(target_physfourvectors[:,a], 150, normed=1, facecolor='green', alpha=0.75)
-        plt.savefig(process+"-cartesian"+str(a)+".pdf")
+        plt.savefig(process+"-cartesian"+str(a)+".png")
--- root_to_csv.py
+++ root_to_csv.py
@@ -6,7 +6,7 @@ filenames = ['ggH', 'DY']
 
 for filename in filenames:
     # Convert a TTree in a ROOT file into a NumPy structured array
-    arr = root2array("data/"+filename + '.root', 'mt_jecUncNom_tauEsNom/ntuple', branches=[
+    arr = root2array("data/"+filename + '.root', 'tt_jecUncNom_tauEsNom/ntuple', branches=[
             "m_sv", "pt_sv", "eta_sv", "phi_sv",
             "m_1", "pt_1", "eta_1", "phi_1",
             "m_2", "pt_2", "eta_2", "phi_2",
@@ -17,11 +17,12 @@ for filename in filenames:
     writer = csv.writer(csvfile, delimiter=';',
                                 quotechar='|', quoting=csv.QUOTE_MINIMAL)
     for a in arr:
-        svfit = FourMomentum(a[0], a[1], a[2], a[3], False)
-        lepton1 = FourMomentum(a[4], a[5], a[6], a[7], False)
-        lepton2 = FourMomentum(a[8], a[9], a[10], a[11], False)
-        met = FourMomentum(0, a[12], 0, a[13], False)
+       # svfit = FourMomentum(a[0], a[1], a[2], a[3], False)
+       # lepton1 = FourMomentum(a[4], a[5], a[6], a[7], False)
+        #lepton2 = FourMomentum(a[8], a[9], a[10], a[11], False)
+       # met = FourMomentum(0, a[12], 0, a[13], False)
 
-        writer.writerow([lepton1.e, lepton1.px, lepton1.py, lepton1.pz, lepton2.e, lepton2.px, lepton2.py, lepton2.pz, met.px, met.py, met.pt2(), svfit.pt, svfit.eta, svfit.phi, svfit.m()])
+        #writer.writerow([lepton1.e, lepton1.px, lepton1.py, lepton1.pz, lepton2.e, lepton2.px, lepton2.py, lepton2.pz, met.px, met.py, met.pt2(), svfit.pt, svfit.eta, svfit.phi, svfit.m()])
+        writer.writerow(a)
     csvfile.close()
 
