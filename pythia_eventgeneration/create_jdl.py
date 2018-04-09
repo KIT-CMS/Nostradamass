1cf97203897d2ef825730836d341c7f68d0f12b0
rfriese <raphael.friese@cern.ch>
2018-04-09 13:08:36 +0200

    refinement of job submission

--- pythia_eventgeneration/create_jdl.py
+++ pythia_eventgeneration/create_jdl.py
@@ -8,18 +8,19 @@ from classes.JDLCreator import JDLCreator  # import the class to create and subm
 
 def main():
     """Submit a simple example job"""
-    out_dir = "/storage/b/friese/toymass9/"
-    jobs = JDLCreator("condocker")  # Default (no Cloud Site supplied): Docker with SLC6 image
+    out_dir = "toymass13"
+    jobs = JDLCreator()  # Default (no Cloud Site supplied): Docker with SLC6 image
     # Some example sites:
     # site_name='ekpsupermachines'  "Super Machines" IO intesiv jobs
 
     jobs.executable = "job.sh"  # name of the job script
     jobs.wall_time = 20 * 60 * 60  # job will finish in 10 hours
-    jobs.memory = 2048  # Our regular 2048 MB per slot
+    jobs.memory = 1900  # Our regular 2048 MB per slot
     jobs.accounting_group = "cms.higgs"
+    jobs.input_files = "/usr/users/friese/toymass/pythia_eventgeneration/eventgeneration,/usr/users/friese/toymass/pythia_eventgeneration/job.sh,/usr/users/friese/.globus/proxy.grid"
 
     arguments = []
-    for masses in range(100,1000):
+    for masses in range(40,1000):
         for seed in [0]:
             for channel in ["tt", "mt", "et", "em", "mm", "ee"]:
                 for invert in ["true", "false"]:
@@ -28,9 +29,11 @@ def main():
     jobs.arguments = arguments  # set arguments for condor job
 
     # Our job requires lots of CPU resources and needs access to the local EKP resources
-    jobs.requirements = "(TARGET.ProvidesCPU ==True) && (TARGET.ProvidesEKPResources == True)"
+    jobs.requirements = "(TARGET.ProvidesCPU ==True)"
 
     jobs.job_folder = "condor_jobs"  # set name of the folder, where files and information are stored
+    jobs.AddExtraLines('+RemoteJob = True')
+    jobs.AddExtraLines('x509userproxy = /usr/users/friese/.globus/proxy.grid')
     jobs.WriteJDL()  # write an JDL file and create folder for log files
     jobs.Submit()
 
--- pythia_eventgeneration/job.sh
+++ pythia_eventgeneration/job.sh
@@ -1,10 +1,40 @@
 #!/bin/bash
-source /cvmfs/sft.cern.ch/lcg/views/LCG_92/x86_64-slc6-gcc7-opt/setup.sh
+ls -al
+ls /cvmfs/sft.cern.ch/
+set -e
 MASS=$1
 SEED=$2
 CHANNEL=$3
 INVERT=$4
 OUT_DIR=$5
-/usr/users/friese/toymass/pythia_eventgeneration/eventgeneration $MASS $SEED $CHANNEL $INVERT >> $OUT_DIR/logfile_${MASS}_${SEED}_${CHANNEL}_${INVERT}.log
-cp *.root $OUT_DIR
+echo "testing if the output works"
+(
+here=$(pwd)
+testfile=${MASS}_${SEED}_${CHANNEL}_${INVERT}.txt
+echo ${MASS}_${SEED}_${CHANNEL}_${INVERT} > $testfile
+source /cvmfs/grid.cern.ch/emi3ui-latest/etc/profile.d/setup-ui-example.sh
+echo "in subshell"
+which python
+echo gfal-copy file:/$here/$testfile srm://cmssrm-kit.gridka.de:8443/srm/managerv2?SFN=/pnfs/gridka.de/cms/disk-only/store/user/rfriese/$OUT_DIR/$testfile
+gfal-copy file:/$here/$testfile srm://cmssrm-kit.gridka.de:8443/srm/managerv2?SFN=/pnfs/gridka.de/cms/disk-only/store/user/rfriese/$OUT_DIR/$testfile
+)
+echo "out of subshell"
+which python
+
+
+(
+source /cvmfs/sft.cern.ch/lcg/views/LCG_92/x86_64-slc6-gcc7-opt/setup.sh
+echo "in subshell2"
+which python
+./eventgeneration $MASS $SEED $CHANNEL $INVERT
+)
+echo "event generation done"
+(
+here=$(pwd)
+rf=$(ls *.root)
+source /cvmfs/grid.cern.ch/emi3ui-latest/etc/profile.d/setup-ui-example.sh
+echo "in subshell3"
+which python
+gfal-copy file:/$here/$rf srm://cmssrm-kit.gridka.de:8443/srm/managerv2?SFN=/pnfs/gridka.de/cms/disk-only/store/user/rfriese/$OUT_DIR/$rf
+)
 
